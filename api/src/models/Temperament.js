4c335de7a831d76018bfcd6c59c98ad803cdf7cf
felihq <felihq24q@gmail.com>
2022-10-25 18:02:47 -0300

    just in case

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -1,11 +1,16 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('temperament', {
-    name: {
+    id: {
       type: DataTypes.STRING,
+      defaultValue: UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.TEXT,
       defaultValue: 'There is no name'
     }
   });
--- api/src/routes/temperamentsRoute.js
+++ api/src/routes/temperamentsRoute.js
@@ -2,22 +2,28 @@ const { Router } = require('express');
 const router = Router();
 const axios = require('axios')
 const api = 'https://api.thedogapi.com/v1/breeds'
+const { Temperament } = require('../db')
+
 
 router.get('/temperaments', async (req, res) => {
   try {
+    const temperamentsFromDB = await Temperament.findAll();
+    if(temperamentsFromDB >= 1) res.send(temperamentsFromDB)
+  
     const apiInfo = await axios.get(api)
-    
-    const convTo = (data) => {
-        if (data) return data.split(', ')
-     }
-    const apiMapeada = apiInfo.data?.map((e) => {
-        return{
-            temperament: convTo(e.temperament)
-        }
-    }) 
-   const mySet = new Set(apiMapeada);
-   mySet? res.status(200).send([...mySet]) : res.status(404).send("No temperaments found")
+    let everyTemperament =  apiInfo.data?.map(dog => dog.temperament ? dog.temperament : null).map(dog => dog && dog.split(', '));
+   const mySet = new Set(everyTemperament.flat());
+   let temperamentsToDB =  mySet.forEach((e) => {
+    if(e){
+      Temperament.findOrCreate({
+        where: { name: e}
+    });
+    }
+   })
+   temperamentsToDB = await Temperament.findAll();
+   res.status(200).send(temperamentsToDB) 
   } catch (error) {
+    res.status(404).send("No temperaments found")
     console.log(error.message)
   }
 })
